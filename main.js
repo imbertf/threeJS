7c817bfa230b2dd0b5e3c0710c40f28870368144
imbertf <f.imbert4@gmail.com>
2023-11-08 16:24:50 +0100

    initial commit

--- main.js
+++ main.js
@@ -4,11 +4,10 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const light = new THREE.AmbientLight( 0x404040, 50 );
+const light = new THREE.AmbientLight( 0x404040, 100 );
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 
