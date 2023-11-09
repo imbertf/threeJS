9a52e8fd76f31d572325ea88d6d190e1c207ad9a
imbertf <100202246+imbertf@users.noreply.github.com>
2023-11-09 15:02:26 +0100

    color selection added

--- main.js
+++ main.js
@@ -1,36 +1,122 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls';
+import { STLLoader } from 'three/addons/loaders/STLLoader.js'
 
+const createOptionsForm = () => {
+    // create form
+    const getOptions = document.getElementById("options")
+    const form = document.createElement("form")
+    getOptions.appendChild(form)
+
+    // create label
+    const getForm = document.querySelector("form")
+    const label = document.createElement("label")
+    label.htmlFor = "color_select"
+    label.textContent = "Color selection"
+    getForm.appendChild(label)
+
+    // create options
+    const colorOptions = [
+        { "value": "#ffffff", "text": "White" },
+        { "value": "#000000", "text": "Black" },
+        { "value": "#d30000", "text": "Red" },
+        { "value": "#008000", "text": "Green" }
+    ]
+
+    colorOptions.map((color) => {
+        const inputColor = document.createElement("input")
+        inputColor.setAttribute("type", "color")
+        inputColor.setAttribute("value", `${color.value}`)
+        form.appendChild(inputColor)
+    });
+};
+
+createOptionsForm();
+
+const selectColor = () => {
+    // get selected color value when clickin on input color
+    const selectedColor = []
+    const inputs = document.querySelectorAll("input")
+    for (let a of inputs) {
+        a.addEventListener("click", function () {
+            if (selectedColor.length = 1) {
+                selectedColor.push(a.value)
+                selectedColor.shift()
+                localStorage.setItem("selectedColor", selectedColor)
+                location.reload()
+            }
+        })
+    }
+}
+
+selectColor();
+
+// DIV for three element
+const container = document.getElementById("canvas")
+
+// scene
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const light = new THREE.AmbientLight( 0x404040, 100 );
+scene.background = new THREE.Color(0xd3d3d3);
+
+// camera using container size
+const camera = new THREE.PerspectiveCamera(30, container.clientWidth / container.clientHeight);
+camera.position.set(2, 5, 10);
+camera.lookAt(scene.position);
+
+// insert renderer in container
 const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+renderer.setSize(container.clientWidth, container.clientHeight);
+renderer.setAnimationLoop(animationLoop);
+container.appendChild(renderer.domElement);
+
+// controls
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 
-camera.position.z = 5;
-scene.background = new THREE.Color( 0xd3d3d3 );
+// light
+const ambientLight = new THREE.AmbientLight('white', 0.5);
+scene.add(ambientLight);
+const light = new THREE.DirectionalLight('white', 0.5);
+light.position.set(1, 1, 1);
+scene.add(light);
 
+// utils
 const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
 
-const loader = new GLTFLoader();
 
-loader.load("./public/mclaren/scene.gltf", function (gltf) {
-    scene.add(gltf.scene);
-}, undefined, function (error) {
-    console.error('Error loading GLB file:', error);
-});
+// cube object
+const cube = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshPhongMaterial({ color: `${localStorage.getItem("selectedColor")}`, shininess: 10 }),
+);
+scene.add(cube)
 
-scene.add( light );
+function animationLoop(t) {
+    // cube.rotation.x = Math.sin(t / 700);
+    // cube.rotation.y = Math.sin(t / 900);
 
-function animate() {
-    requestAnimationFrame(animate);
-    controls.update()
+    controls.update();
+    light.position.copy(camera.position);
     renderer.render(scene, camera);
 }
 
-animate();
+
+
+// GLTF FORMAT
+// const loader = new GLTFLoader();
+// loader.load("./public/mclaren/scene.gltf", function (gltf) {
+//     scene.add(gltf.scene);
+// }, undefined, function (error) {
+//     console.error('Error loading GLB file:', error);
+// });
+
+// STL FORMAT
+// const loader = new STLLoader();
+// loader.load('./public/oeweo/scene.stl', function (stl) {
+//     scene.add(stl.scene);
+// }, undefined, function (error) {
+//     console.error(error)
+// }
+// )
